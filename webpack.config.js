57b5c78cf28f9eef1ddc7649bce19dfa48c9b83f
Elise Carman <elise.carman1@gmail.com>
2023-09-21 12:43:33 -0400

    file loader shenanigans

--- webpack.config.js
+++ webpack.config.js
@@ -19,13 +19,10 @@ module.exports = {
                 ]
             }, {
                 test: /\.css$/,
-                use:{
+                use:[{
                 loader: ['style-loader', 'css-loader']
-                }
-            }, {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
+                }]
+            }, 
         ]
     },
     plugins: [
