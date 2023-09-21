fe2d854071695cc4beec25ddba47e828c5debf3a
Elise Carman <elise.carman1@gmail.com>
2023-09-21 11:52:35 -0400

    oops

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/app.js",
+    entry: "./src/index.js",
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
