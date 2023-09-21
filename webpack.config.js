69b6f4a7f8b3d2c18875022afbf87dfbd48a7a71
Elise Carman <elise.carman1@gmail.com>
2023-09-21 12:08:13 -0400

    specified loaders for css and images

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,12 @@ module.exports = {
                         loader: "html-loader"
                     }
                 ]
+            }, {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }, {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                loader: 'url-loader?limit=100000'
             }
         ]
     },
